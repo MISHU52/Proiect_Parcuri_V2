7209046da410e049cf9f503a1dfec13879fbceda
Mihai Constantinescu <mihai.constantinescu@stud.mta.ro>
2026-04-28 08:01:30 +0300

    Curatare fisier build

--- build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/.qt/deploy_Gestionare_parcuriV2_78d9a9738e.cmake
+++
@@ -1,8 +0,0 @@
-include("C:/Qt_work/Gestionare_parcuriV2/build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/.qt/QtDeploySupport.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/Gestionare_parcuriV2-plugins.cmake" OPTIONAL)
-set(__QT_DEPLOY_I18N_CATALOGS "qtbase")
-
-qt6_deploy_runtime_dependencies(
-    EXECUTABLE "C:/Qt_work/Gestionare_parcuriV2/build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/Gestionare_parcuriV2.exe"
-    GENERATE_QT_CONF
-)
--- build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/CMakeFiles/Gestionare_parcuriV2_autogen_timestamp_deps.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for Gestionare_parcuriV2_autogen_timestamp_deps.
-# This may be replaced when dependencies are built.
